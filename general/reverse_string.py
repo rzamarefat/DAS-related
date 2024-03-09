829355bb5e1f6fac461f1c256014763d345c8b8d
rzamarefat <rzamarefat@gmail.com>
2024-03-09 17:37:59 +0330

    progress

---
+++ general/reverse_string.py
@@ -0,0 +1,13 @@
+"""
+the sky is blue -> blue is sky the
+"""
+
+
+def reverse_string(string):
+    words = [w.strip() for w in string.split(" ")]
+
+    return " ".join(words[::-1])
+
+if __name__ == "__main__":
+    res = reverse_string("the sky is blue")
+    print(res)
